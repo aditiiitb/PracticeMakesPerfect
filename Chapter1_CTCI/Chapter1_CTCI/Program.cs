3f1b264ff9440cf8e472c672310921c6072b961d
Aditi Goyal <adig@microsoft.com>
2014-07-24 14:22:59 -0700

    Added reverse string method

--- Chapter1_CTCI/Chapter1_CTCI/Program.cs
+++ Chapter1_CTCI/Chapter1_CTCI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,32 @@ namespace Chapter1_CTCI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string s = "abcde";
+            Debug.WriteLine(s);
+            Debug.WriteLine(Reverse(s));
+        }
+
+        public static string Reverse(string s)
+        {
+            int n = s.Length;
+            for (int i = 0; i < n/2; i++)
+            {
+                s = Swap(s.ToCharArray(), i, n - 1);
+            }
+            return s;
+        }
+
+        public static string Swap(char[] s, int i, int j)
+        {
+            if (i < s.Length && i > 0 && j < s.Length && j > 0)
+            {
+                char a = s[i];
+                s[i] = s[j];
+                s[j] = a;
+            }
+            return s.ToString();
         }
     }
 }
