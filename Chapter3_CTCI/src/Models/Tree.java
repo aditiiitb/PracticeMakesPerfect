22d4c025228962ee8b13240722811981705dff1e
Aditi Goyal <adig@microsoft.com>
2014-08-04 14:21:17 -0700

    added bfs, dfs

--- Chapter3_CTCI/src/Models/Tree.java
+++ Chapter3_CTCI/src/Models/Tree.java
@@ -1,39 +1,26 @@
 package Models;
 
+
+import java.util.*;
+
+
 public class Tree {
 	public Node Head;
 	
-	public Tree()
-	{		
-		Head = CreateTree(5);
-		PrintTreeInOrder(Head);
+	public Tree(Integer[] arr)
+	{				
+		Head = CreateTreeFromArray(arr, 0, arr.length-1);		
 	}
 	
-	public void InsertNode(Node node)
+	public boolean BFS(int val)
 	{
-		
+		return BFSHelper(Head, val);
 	}
-	
-	public void RemoveNode(Node node)
+	public boolean DFS(int val)
 	{
-		
+		return DFSHelper(Head, val);
 	}
-	
-	
-	private Node CreateTree(int val)
-	{
-		if (val <=0 || val >=10)
-		{
-			return null;
-		}
-		Node root = new Node();
-		root.Value = val;
-		root.Left = CreateTree(val -1);
-		root.Right = CreateTree(val +1);		
-		return root;
-	}
-	
-	private void PrintTreeInOrder(Node x)
+	public void PrintTreeInOrder(Node x)
 	{
 		if (x!= null)
 		{
@@ -42,4 +29,79 @@ public class Tree {
 			PrintTreeInOrder(x.Right);
 		}
 	}
+	public void PrintPretty()
+	{
+		Node SpecialNode = new Node();
+		SpecialNode.Value = 1000;
+		
+		if (Head == null)
+			return;
+		// prints in a tree format
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(Head);
+		q.add(SpecialNode);
+		while (!q.isEmpty())
+		{
+			System.out.println(); // print for next line			
+			while (q.peek() != SpecialNode)
+			{
+				Node s = q.remove();
+				if (s.Left != null)
+					q.add(s.Left);
+				if (s.Right != null)
+					q.add(s.Right);
+				System.out.print(s.Value + " ");
+			}
+			q.add(SpecialNode);// add the special char to end of queue		
+			q.remove();// remove the special node from head of queue				
+		}
+	}
+	
+	
+	
+	private boolean DFSHelper(Node head, int val)
+	{
+		if (head==null)
+			return false;
+		if (head.Value == val)
+			return true;
+		else return DFSHelper(head.Left,val) || DFSHelper(head.Right, val);
+	}
+	private Node CreateTreeFromArray(Integer[] vals, int start, int end)
+	{
+		if (start > end)
+			return null;
+		
+		Node root = new Node();
+		root.Value = vals[(start+end)/2];
+		root.Left = CreateTreeFromArray(vals, start, (start+end)/2 - 1);
+		root.Right = CreateTreeFromArray(vals, (start+end)/2 +1 , end);
+		return root;
+	}
+	private boolean BFSHelper(Node head, int val)
+	{
+		if (head == null)
+			return false;
+		
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(head);
+		while (!q.isEmpty())
+		{
+			Node root = q.remove();
+			System.out.print(root.Value + " ");
+			if (root.Value == val)
+					return true;
+				else 
+				{
+					if (root.Left != null)
+						q.add(root.Left);
+					if (root.Right != null)						
+						q.add(root.Right);
+				}
+		}
+		
+		return false;
+	}
+	
+
 }
--- Chapter3_CTCI/src/Program.java
+++ Chapter3_CTCI/src/Program.java
@@ -5,8 +5,16 @@ public class Program {
 	public static void main(String args[])
 	{
 		System.out.println("Dzsdd");
+		Integer[] arr = {1,2,3,4,5,6,7,8,9,10};
+		Tree z = new Tree(arr);
+		z.PrintTreeInOrder(z.Head);
+		System.out.println();
+		System.out.println( "11 " + z.DFS(11));
+		System.out.println( "9 " + z.DFS(9));
 		
-		Tree z = new Tree();
-		System.out.println(z.Head.Value);
+		System.out.println( "11 " + z.BFS(11));
+		System.out.println( "9 " + z.BFS(9));
+		
+		z.PrintPretty();
 	}
 }
